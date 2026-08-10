40b0abb2fb1efaf4fe5f257418535808112ccfb4
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:10:05 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/dingtalk/token/DingTalkScanCodeAuthenticationToken.java
+++ src/main/java/org/apache/shiro/spring/boot/dingtalk/token/DingTalkScanCodeAuthenticationToken.java
@@ -22,7 +22,7 @@ import org.apache.shiro.spring.boot.dingtalk.authc.DingTalkScanCodeLoginRequest;
 /**
  * DingTalk Authentication Token
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @SuppressWarnings("serial")
 public class DingTalkScanCodeAuthenticationToken extends DefaultAuthenticationToken {
