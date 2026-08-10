671a55c11850f2e71987e0c1e077ee2242aec491
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:09:23 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/dingtalk/realm/DingTalkTempCodeAuthorizingRealm.java
+++ src/main/java/org/apache/shiro/spring/boot/dingtalk/realm/DingTalkTempCodeAuthorizingRealm.java
@@ -16,7 +16,7 @@ import org.springframework.util.StringUtils;
 
 /**
  * DingTalk AuthorizingRealm
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @Slf4j
 public class DingTalkTempCodeAuthorizingRealm extends AbstractAuthorizingRealm {
